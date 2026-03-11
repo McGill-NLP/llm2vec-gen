34bbe59e7958579fd16389a9d5586060ad2798c8
vaibhavad <vaibhavadlakha95@gmail.com>
2026-03-11 12:16:28 +0000

    [Minor Change] Added sample queries to LatentLens index

--- scripts/latent_lens_build_index.py
+++ scripts/latent_lens_build_index.py
@@ -5,8 +5,9 @@ import json
 tulu = load_dataset("McGill-NLP/llm2vec-gen-tulu", split="Qwen3_8B")
 tulu = tulu.shuffle()
 tulu = tulu.select(range(150000))
-test_samples = json.load(open("scripts/latentlens_qwen3_8b_generations.json"))
-test_samples = [sample["response"] for sample in test_samples]
+samples = json.load(open("scripts/latentlens_qwen3_8b_generations.json"))
+test_samples = [sample["response"] for sample in samples]
 corpus = list(tulu["answer"] + test_samples)
+corpus = corpus + list(set([sample["query"] for sample in samples]))
 index = latentlens.build_index("Qwen/Qwen3-8B", corpus=corpus)
-index.save("qwen3-8b_index/")
+index.save("qwen3-8b_index_with_queries/")
